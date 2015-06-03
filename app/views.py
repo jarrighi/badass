ee863ced17c6050a1f6c8ca37cd4654140063a84
Jules Arrighi <juliana.arrighi@gmail.com>
2015-06-02 23:56:46 -0700

    piles page

--- app/views.py
+++ app/views.py
@@ -23,3 +23,16 @@ def add_skill():
   db.session.add(skill_obj)
   db.session.commit()
   return redirect(url_for('skills'))
+
+@app.route('/piles')
+def piles():
+  titles = {1: "can't do", 3: "can do with effort", 5: "mastered"}
+  result = [get_pile_data(x, titles[x]) for x in (1,3,5)]
+  return render_template('piles.html', data=result)
+
+def get_pile_data(level, name):
+  result = {'title':name, 'skills': []}
+  query = models.Skill.query.filter(models.Skill.level==level)
+  for skill in query:
+    result['skills'].append(skill)
+  return result
